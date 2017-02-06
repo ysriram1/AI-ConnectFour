f6cf659e07ac7a20d030e9d93bef90e0561c1725
ysriram1 <ysriram@umich.edu>
2017-02-05 19:37:21 -0600

    start

---
+++ script.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Feb 05 18:58:50 2017
+
+@author: Sriram Yarlagadda
+"""
+import os
+import math
+os.chdir('C:/Users/SYARLAG1/Desktop/AI-ConnectFour')
+
+
+# 9 x 9 board
+# 2 points for pair next to each other
+# 1 point for every piece diagnol to each other
+
+class board():
+    
+    def printBoard:
+        
